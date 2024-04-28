8ed174cea1b644c51f7626cb03dd6ff45b0c5612
kobi <kobimic@gmail.com>
2024-04-28 17:35:44 +0300

    fail a test condition

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -3,5 +3,11 @@ from my_package.app import Demo
 
 def test_app():
     x = Demo(name="kobi", age=5)
-    assert x.age==5
-    assert x.name=="kobi"
+    assert x.age == 5
+    assert x.name == "kobi"
+
+
+def test_app_2():
+    x = Demo(name="kobi", age=5)
+    assert x.age == 6
+    assert x.name == "john"
